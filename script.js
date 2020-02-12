a52d12adcf622bf2e715304081820aa804c201d0
Tosha Shigenina <shigenina@gmail.com>
2020-02-12 23:38:55 +0500

    Создан класс и его метод для создания елемента

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ function DomElement(selector, height, width, bg, fontSize) {
 }
 
 DomElement.prototype.createElem = function () {
-  if (this.selector.indexOf('.') === 0) {
+  let elem;
 
+  if (this.selector[0] === '.') {
+    elem = document.createElement('div');
+    elem.classList.add(this.selector.slice(1));
   }
+
+  if (this.selector[0] === '#') {
+    elem = document.createElement('p');
+    elem.createAttribute(this.selector.slice(1));
+  }
+
+  return elem;
 };
+
+let div = new DomElement('.block', 185, 103, '#ccc');
+
+console.log(div.createElem());

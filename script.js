f160d5eace0fa57f3bd4eca75a8aa928cef50f75
Tosha Shigenina <shigenina@gmail.com>
2020-02-13 00:44:32 +0500

    Квадрат ходит)

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
+  this.top = 0;
+  this.left = 0;
 }
 
 DomElement.prototype.createElem = function () {
@@ -27,7 +30,7 @@ DomElement.prototype.createElem = function () {
 };
 
 DomElement.prototype.styling = function (elem) {
-  elem.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
+  elem.style.cssText = `position: ${this.position}; height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}`;
 };
 
 DomElement.prototype.writeText = function (elem) {
@@ -39,4 +42,48 @@ DomElement.prototype.addElem = function (elem) {
 };
 
 
-let square = new DomElement('.block', '185px', '103px', '#ccc', '2rem');
+let square = new DomElement('.square', '100px', '100px', '#ccc', '10px', 'absolute');
+
+document.addEventListener('DOMContentLoaded', function () {
+  square.createElem();
+});
+
+document.body.addEventListener('keydown', function (event) {
+  let squareCopy = document.querySelector(square.selector);
+
+  if (event.key === 'ArrowUp') {
+    if (square.top - 10 > 0) {
+      squareCopy.style.top = `${square.top-10}px`;
+      square.top -= 10;
+    }
+    /*else {
+      squareCopy.style.top = `0`;
+      square.top = 0;
+    }*/
+  }
+
+  if (event.key === 'ArrowDown') {
+    if (square.top + 110 < window.innerHeight) {
+      squareCopy.style.top = `${square.top+10}px`;
+      square.top += 10;
+    }
+  }
+
+  if (event.key === 'ArrowLeft') {
+    if (square.left - 10 > 0) {
+      squareCopy.style.left = `${square.left-10}px`;
+      square.left -= 10;
+    }
+    /*else {
+      squareCopy.style.left = `0`;
+      square.left = 0;
+    }*/
+  }
+
+  if (event.key === 'ArrowRight') {
+    if (square.left + 110 < window.innerWidth) {
+      squareCopy.style.left = `${square.left+10}px`;
+      square.left += 10;
+    }
+  }
+});
